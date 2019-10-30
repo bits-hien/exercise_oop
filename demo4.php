bf6aa491593e992f21f2d5e45c497df5da7586ab
bits-hien <=>
2019-10-30 16:08:24 +0900

    next oop

--- demo4.php
+++ demo4.php
@@ -1,49 +1,49 @@
 <?php
-    class Tam_giac{
-        public $chieu_cao;
-        public $canh_day;
-        public $canh_ke1;
-        public $canh_ke2;
+    class Triangle{
+        public $height;
+        public $bottom_edge;
+        public $adjacent_side1;
+        public $adjacent_side2;
 
-        function __construct($canh_day, $canh_ke1, $canh_ke2, $chieu_cao){
-            $this->chieu_cao = $chieu_cao;
-            $this->canh_day = $canh_day;
-            $this->canh_ke1 = $canh_ke1;
-            $this->canh_ke2 = $canh_ke2;
+        function __construct($bottom_edge, $adjacent_side1, $adjacent_side2, $height){
+            $this->height = $height;
+            $this->bottom_edge = $bottom_edge;
+            $this->adjacent_side1 = $adjacent_side1;
+            $this->adjacent_side2 = $adjacent_side2;
         }
 
-        function chu_vi(){
-            return $this->canh_day + $this->canh_ke1 + $this->canh_ke2;
+        function perimeter_triangle(){
+            return $this->bottom_edge + $this->adjacent_side1 + $this->adjacent_side2;
         }
 
-        function dien_tich(){
-            return $this->canh_day * $this->canh_ke1 /2;
+        function acreage_triangle(){
+            return $this->bottom_edge * $this->height /2;
         }
     } 
-    $GT1 = new Tam_giac(5, 6, 7, 6);
-    echo "Chu vi của hình tam giác đó là: ". $GT1-> chu_vi(). "cm";
+    $GT1 = new Triangle(5, 6, 7, 6);
+    echo "Perimeter_triangle is: ". $GT1-> perimeter_triangle(). "cm";
     echo "<br>";
-    echo "Diện tích hình tam giác đó là: ". $GT1-> dien_tich(). "cm2";
+    echo "perimeter_triangle is ". $GT1-> acreage_triangle(). "cm2";
     echo "<br>";
 
 
-    class Hinh_vuong {
-        public $canh;
+    class Square {
+        public $sqare_edge;
 
-        function __construct($canh){
-            $this->canh = $canh;
+        function __construct($square_edge){
+            $this->square_edge = $square_edge;
         }
 
-        function chu_vi_HV(){
-            return $this->canh *4;
+        function perimeter_square(){
+            return $this->square_edge *4;
         }
 
-        function S(){
-            return $this->canh * $this->canh;
+        function acreage_square(){
+            return $this->square_edge * $this->square_edge;
         }
     }
-    $GT2 = new Hinh_vuong(24);
-    echo "Chu vi của hình vuông đó là: ". $GT2->chu_vi_HV(). "cm";
+    $GT2 = new Square(24);
+    echo "Chu vi của hình vuông đó là: ". $GT2->perimeter_square(). "cm";
     echo "<br>";
-    echo "Diện tích hình vuông đó là: ". $GT2->S() . "cm2";
+    echo "Diện tích hình vuông đó là: ". $GT2->acreage_square() . "cm2";
 ?>
